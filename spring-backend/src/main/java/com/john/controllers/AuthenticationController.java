444c5979988596f233079efc11639233fbc376f1
ljn7 <johnnirmal.16897@gmail.com>
2024-06-03 16:43:06 +0530

    Bundle commit

---
+++ spring-backend/src/main/java/com/john/controllers/AuthenticationController.java
@@ -0,0 +1,50 @@
+package com.john.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.john.models.RegistrationDTO;
+import com.john.models.UserJwtResponse;
+import com.john.services.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegistrationDTO body){
+
+        try {
+            var res = authenticationService.registerUser(body.getUsername(), body.getPassword());
+            return ResponseEntity.ok(res);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Username is already taken!");
+        } 
+        
+        
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody RegistrationDTO body){
+        
+        
+        try {
+            var res = authenticationService.loginUser(body.getUsername(), body.getPassword());
+
+            return ResponseEntity.ok().body(new UserJwtResponse(res.getUser().getUsername(),
+                 res.getJwt()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Internal Server Error");
+        }
+    }
+}   
+
